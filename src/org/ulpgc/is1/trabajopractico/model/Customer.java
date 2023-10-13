ed1826939f90f384de1f59d56ed14fc7bd953645
Dani <a@a.com>
2023-10-13 09:31:01 +0100

    Implementacion de Customer(sin terminar)

--- src/org/ulpgc/is1/trabajopractico/model/Customer.java
+++ src/org/ulpgc/is1/trabajopractico/model/Customer.java
@@ -1,4 +1,11 @@
 package org.ulpgc.is1.trabajopractico.model;
 
+import java.util.ArrayList;
+
 public class Customer {
+    public ArrayList<Address> address;
+    public String name, surname;
+    }
+
+    void addAddress()
 }
