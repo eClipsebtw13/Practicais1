c3112e61ce4d63e3c6018a5be573c4c686b74b02
Dani <a@a.com>
2023-10-13 09:39:20 +0100

    Class Customer added

--- src/org/ulpgc/is1/trabajopractico/model/Customer.java
+++ src/org/ulpgc/is1/trabajopractico/model/Customer.java
@@ -5,7 +5,27 @@ import java.util.ArrayList;
 public class Customer {
     public ArrayList<Address> address;
     public String name, surname;
+
+    void addAddress(String street, String city, int number, int postalCode) {
+    Address a = new Address (stree, city, number, postalCode);
+    address.add(a);
     }
 
-    void addAddress()
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
 }
+
+
